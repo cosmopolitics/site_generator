bbee9d6e1e4be438489b245792ee65b5ed35e64b
Cosmo waterbury <cosmowaterbury@gmail.com>
2025-10-04 02:46:16 -0700

    init

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages.default = pkgs.stdenv.mkDerivation {
+          name = "site_gen";
+          src = ./.;
+          buildInputs = [ pkgs.uv ];
+          buildPhase = ''
+          '';
+          installPhase = ''
+            mkdir -p $out/bin
+            cp output $out/bin
+          '';
+        };
+
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            uv
+            python3
+          ];
+        };
+        formatter = pkgs.alejandra;
+      }
+    );
+}
