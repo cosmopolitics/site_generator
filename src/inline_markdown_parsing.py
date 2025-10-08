ef6f31dd6b12b487e69627d21ccb960753513fec
Cosmo waterbury <cosmowaterbury@gmail.com>
2025-10-07 23:34:38 -0700

    extract links and images

--- src/inline_markdown_parsing.py
+++ src/inline_markdown_parsing.py
@@ -1,9 +1,10 @@
+import re
 from src.textnode import TextNode, TextType
 
 
 def split_nodes_delimiter(
     old_nodes: list[TextNode], delimiter: str, text_type: TextType
-):
+) -> list[TextNode]:
     new_nodes: list[TextNode] = []
     for node in old_nodes:
         if node.text_type is not TextType.text:
@@ -21,3 +22,13 @@ def split_nodes_delimiter(
                 new_nodes.append(TextNode(split_text[i], TextType.text, None))
 
     return new_nodes
+
+def extract_markdown_images(text: str) -> list[tuple[str, str]]:
+    pattern = r"!\[([^\[\]]*)\]\(([^\(\)]*)\)"
+    matches = re.findall(pattern, text)
+    return matches
+
+def extract_markdown_links(text: str) -> list[tuple[str, str]]:
+    pattern = r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)"
+    matches = re.findall(pattern, text)
+    return matches
--- src/tests/test_inline_markdown.py
+++ src/tests/test_inline_markdown.py
@@ -1,6 +1,8 @@
 import unittest
-from src.split_node_delimiter import (
+from src.inline_markdown_parsing import (
     split_nodes_delimiter,
+    extract_markdown_images,
+    extract_markdown_links,
 )
 
 from src.textnode import TextNode, TextType
@@ -86,6 +88,18 @@ class TestInlineMarkdown(unittest.TestCase):
             new_nodes,
         )
 
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_extract_markdown_links(self):
+        matches = extract_markdown_links(
+            "This is text with an [link to imgur](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("link to imgur", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
 
 if __name__ == "__main__":
-    unittest.main()
+    _ = unittest.main()
